53ea63f094cb87f310d4baee5a51c7052aa607e0
MattAlp <alpmatthew@gmail.com>
2018-04-17 19:05:00 -0400

    Usability updates
    AKA idiot-proofing

--- core/config.py
+++ core/config.py
@@ -1,5 +1,5 @@
 # Edit config values as needed here
-STREAM_KEY = "GHQTVWYZ4"
-STEAM_64_ID = "76561198068710988"
-DISCORD_CLIENT_ID = "431634135811883019"
-UPDATE_INTERVAL = 1  # How long the script should wait between updates
+STREAM_KEY = "GHQTVWYZ4"  # Your Stream Key from within Nuclear Throne, must be re-enabled each game launch
+STEAM_64_ID = "76561198068710988"  # Your Steam64ID, see the readme
+DISCORD_CLIENT_ID = "431634135811883019"  # Do not change this unless you are a developer
+UPDATE_INTERVAL = 1  # How long the script should wait between updates, do not change unless you are a developer
--- main.py
+++ main.py
@@ -5,15 +5,18 @@ from discoIPC import ipc
 
 if __name__ == "__main__":
     print("Starting Nuclear Throne RPC")
-    client = ipc.DiscordIPC(DISCORD_CLIENT_ID)
-    client.connect()
-    if STREAM_KEY != "" and STEAM_64_ID != "":
-        s = Session(STREAM_KEY, STEAM_64_ID)
-    else:
-        print("[IMPORTANT]: Either your stream key or your Steam64ID are not set, please update config.py")
+
+    if input("Please confirm that your Stream Key is %s, type Y to continue\n" % STREAM_KEY).lower() != "y":
+        exit()
+    if input("Please confirm that your Steam64ID is %s, type Y to continue\n" % STEAM_64_ID).lower() != "y":
         exit()
 
+    s = Session(STREAM_KEY, STEAM_64_ID)
+    client = ipc.DiscordIPC(DISCORD_CLIENT_ID)
+    client.connect()
+
     print("Started Nuclear Throne RPC")
+
     while True:
         try:
             s.update()
